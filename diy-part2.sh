21ef2ed94febf88b5865ca1e28eca4bd69795821
SZ-MB <49933875+SZ-MB@users.noreply.github.com>
2022-03-01 14:34:07 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,15 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/zh_cn/auto/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/Shanghai/Taipei/g' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/zh_cn/auto/g' feeds/luci/modules/luci-base/root/etc/uci-defaults/luci-base
+sed -i 's/192.168.1.1/192.168.6.5/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/Github_SZ-MB/g' package/base-files/files/bin/config_generate
+sed -i 's/ntp.aliyun.com/time.windows.com/g' package/base-files/files/bin/config_generate
+sed -i 's/time1.cloud.tencent.com/time.cloudflare.com/g' package/base-files/files/bin/config_generate
+sed -i 's/time.ustc.edu.cn/time1.google.com/g' package/base-files/files/bin/config_generate
+sed -i 's/cn.pool.ntp.org/time.apple.com/g' package/base-files/files/bin/config_generate
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' ./feeds/luci/collections/luci/Makefile
