5c894586bdd2b146b939410ef2d3d976324ae34b
SZ-MB <49933875+SZ-MB@users.noreply.github.com>
2022-03-25 00:52:13 +0000

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-sed -i 's/5.4/5.10/g' ./target/linux/ramips/Makefile
+
